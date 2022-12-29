131a6939f9f0d1cc5ddd5c5c23d39973e4772edc
Gabriel Sardina <gabinex15@gmail.com>
2022-12-29 15:26:08 -0300

    add start project

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export const CounterApp = () => {
+	const [state, setCounter] = useState({
+		counter1: 10,
+		counter2: 20,
+		counter3: 30,
+	});
+	const { counter1, counter2, counter3 } = state;
+
+	const onSumar = () => {
+		console.log("OnSumar");
+		console.log(counter1, counter2, counter3);
+		setCounter({ ...state, counter1: counter1 + 1 });
+	};
+
+	return (
+		<>
+			<h1>Counter1: {counter1} </h1>
+			<h1>Counter2: {counter2} </h1>
+			<h1>Counter3: {counter3} </h1>
+			<hr />
+			<button className="btn btn-primary btn-lg" onClick={onSumar}>
+				+1
+			</button>
+			<button
+				className="btn btn-primary btn-lg"
+				onClick={() => setCounter({ ...state, counter2: counter2 + 1 })}
+			>
+				+1
+			</button>
+			<button
+				className="btn btn-primary btn-lg"
+				onClick={() => setCounter({ ...state, counter3: counter3 + 1 })}
+			>
+				+1
+			</button>
+		</>
+	);
+};
